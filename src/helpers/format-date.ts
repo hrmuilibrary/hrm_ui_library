faa2110d1f822bae6d4978191b256cdbd1618773
manuktagushyan <manuk.tagushyan@ameriabank.am>
2025-06-26 10:16:46 +0400

    fix: add utc format helper, update input disabled state icon colors

--- src/helpers/format-date.ts
+++ src/helpers/format-date.ts
@@ -1,5 +1,8 @@
 ﻿import dayjs from 'dayjs'
+import utc from 'dayjs/plugin/utc'
 import { getDayJSLocale } from './locale'
+
+dayjs.extend(utc)
 
 export const formatDate = (
   value: string | Date | null | undefined,
@@ -12,4 +15,17 @@ export const formatDate = (
   }
 
   return dayjs(value).locale(getDayJSLocale()).format(format)
+}
+
+export const formatDateUTC = (
+  value: string | Date | null | undefined,
+  format: string,
+  defaultValue = ''
+): string => {
+  const isValid = dayjs(value).isValid()
+  if (!isValid) {
+    return defaultValue
+  }
+
+  return dayjs.utc(value).locale(getDayJSLocale()).format(format)
 }
