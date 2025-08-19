482c141fdb668f85ee7a2524914ea8697f82906c
gohar <gohar.gevorgyan@ameriabank.am>
2025-08-19 17:44:07 +0400

    fix: resolve key of CardGroup component

--- src/components/CardGroup/CardGroupComponent/CardBody.tsx
+++ src/components/CardGroup/CardGroupComponent/CardBody.tsx
@@ -1,13 +1,17 @@
-import React, { useMemo } from 'react'
+import React, { useId, useMemo } from 'react'
 
 export const CardGroupBody = ({ children }: { children: React.ReactNode }) => {
   const isChildrenArray = useMemo(() => Array.isArray(children), [children])
 
+  const componentKey = useId()
+
   return (
     <div className="card--group__content--body">
       {isChildrenArray &&
-        (children as Array<React.ReactNode>)?.map((child) => (
-          <div className="card--group__child">{child}</div>
+        (children as Array<React.ReactNode>)?.map((child, id) => (
+          <div className="card--group__child" key={`${componentKey}_${id}`}>
+            {child}
+          </div>
         ))}
       {!isChildrenArray && children}
     </div>
