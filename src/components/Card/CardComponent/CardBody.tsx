fa7392577074c85faaba604e7db007cfd184a0fe
manuktagushyan <manuk.tagushyan@ameriabank.am>
2025-08-25 14:02:05 +0400

    fix: update framer-motion library

--- src/components/Card/CardComponent/CardBody.tsx
+++ src/components/Card/CardComponent/CardBody.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { motion } from 'framer-motion'
+import { motion } from 'motion/react'
 import { useCardContext } from '../CardContext'
 import { Divider } from '../../Divider'
 import { AnimatePresenceWrapper } from '../../../helperComponents/AnimatePresenceWrapper'
