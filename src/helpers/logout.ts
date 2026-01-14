15905647abecf5426f0f7797b7761c8105da9218
LilitArz <lilitarzzz@gmail.com>
2026-01-14 10:47:00 +0400

    prettier fix

--- src/helpers/logout.ts
+++ src/helpers/logout.ts
@@ -1,7 +1,7 @@
 import { clearStorage } from '../utils/helpers'
 import { getModulePrefix } from './get-module-prefix'
 
-export const logout = (path: string = "/auth/logout") => {
+export const logout = (path: string = '/auth/logout') => {
   clearStorage()
   const currentUrl = `${window.location.pathname}${window.location.search}`
   const encodedReturnUrl = encodeURIComponent(currentUrl)
