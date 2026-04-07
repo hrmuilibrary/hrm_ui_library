e6a44261dfb05592d1b3fb6ff5cc79d08b25b895
Gohar Gevorgyan <user@InfoSecs-MacBook-Pro.local>
2026-04-07 13:30:35 +0400

    fix: change getCurrentWeekWorkdaysRange function direction

--- src/components/Select/Select/helpers.ts
+++ src/components/Select/Select/helpers.ts
@@ -11,13 +11,3 @@ export const filterSearchData = (data: TSelectOptions, searchString: string): TS
   }
   return data.filter(({ label }) => containsSearchString(label, searchString))
 }
-
-export const getCurrentWeekWorkdaysRange = (): [Date, Date] => {
-  const today = new Date()
-  const start = new Date(today)
-  const end = new Date(today)
-  const day = today.getDay()
-  const diffToMonday = day === 0 ? -6 : 1 - day
-  start.setDate(today.getDate() + diffToMonday)
-  return [start, end]
-}
