13b61a82a37f49a34bfb611254f0fc92f83ffe71
Gohar Gevorgyan <user@InfoSecs-MacBook-Pro.local>
2026-04-22 16:22:31 +0400

    fix: update Link component to have different types

--- src/components/Link/types.ts
+++ src/components/Link/types.ts
@@ -1,6 +1,21 @@
+type TLinkTypes =
+  | 'primary'
+  | 'secondary'
+  | 'tertiary'
+  | 'disabled'
+  | 'inverse'
+  | 'selected'
+  | 'brand'
+  | 'danger'
+  | 'warning'
+  | 'success'
+  | 'information'
+  | 'discovery'
+
 export interface LinkPropTypes {
   children?: string | React.ReactElement
   className?: string
+  type?: TLinkTypes
   url?: string
   dataId?: string
   target?: string
